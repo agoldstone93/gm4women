157ce96f1f1ad3da6791e729609935517dc853ce
Adam Goldstone <amg93@msn.com>
2020-08-03 12:46:25 +0100

    removing gridlines from graph

--- src/components/graph.js
+++ src/components/graph.js
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, {Component} from 'react'
+import Chart from 'chart.js'
 
-function Graph () {
-    return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
-    )
+
+export default class LineGraph extends Component {
+    chartRef = React.createRef();
+    
+    componentDidMount() {
+        const myChartRef = this.chartRef.current.getContext("2d");
+        
+        new Chart(myChartRef, {
+            type: 'bar',
+            data: {
+                labels: ['Employment', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                    'rgba(255, 99, 132, 0.4)',
+                    'rgba(54, 162, 235, 0.4)',
+                    'rgba(255, 206, 86, 0.4)',
+                    'rgba(75, 192, 192, 0.4)',
+                    'rgba(153, 102, 255, 0.4)',
+                    'rgba(255, 159, 64, 0.4)'
+                ]
+            }]
+        },
+            options: {
+                legend: {
+                    display: true
+                },
+                scales: {
+                    yAxes: [{
+                        gridLines: {
+                            display: false
+                        },
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }],
+                    xAxes: [{
+                        gridLines: {
+                            display: false
+                        }
+                    }]
+                }
+            }
+        });
+    }
+    render() {
+        return (
+            <div>
+                <canvas
+                    id="myChart"
+                    ref={this.chartRef}
+                />
+            </div>
+        )
+    }
 }
-
-export default Graph
