92c7e0657b692350853e918b54c099fece085ad6
Adam Goldstone <amg93@msn.com>
2020-08-20 18:46:12 +0100

    updating icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,12 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Graph from "../components/graph"
 
-const FullWidthColor = styled.div `
+const FullWidthColor = styled.div`
   margin: 0 -9999rem;
   padding: 0.25rem 9999rem;
-  background: lightgrey; 
+  background: lightgrey;
 `
-const TopFullWidthColor = styled(FullWidthColor) `
+const TopFullWidthColor = styled(FullWidthColor)`
   height: 90vh;
 `
 const BigText = styled.h1`
@@ -37,51 +37,46 @@ class IndexPage extends React.Component {
           </BigText>
         </TopFullWidthColor>
 
-        
-        
-        
-          <h2>
-            We collect data to produce our annual scorecard, which shows how
-            equal society is for women in Greater Manchester
-          </h2>
-          <p>
-            We focus collecting and reporting in 5 main areas: education,
-            society, health and culture, participation and employment.
-          </p>
-          <p>
-            We recruit local women's organisations to participate so we have
-            accurate information.
-          </p>
-          <h1>Who we are</h1>
-          <p>
-            Led by Helen Pankhurst, we are a coalition of change-makers
-            committed to seeing a better future for the women of Manchester. ​​
-          </p>
-          <p>
-            As a region that has always led the way and flown the flag for
-            women’s rights, our first initiative, the Pankhurst-Fawcett
-            Scorecard will once again see Greater Manchester leading the charge
-            to bring progress and accountability for women’s rights.
-          </p>
-        
+        <h1>
+          We collect data to produce our annual scorecard, which shows how equal
+          society is for women in Greater Manchester
+        </h1>
+        <h1 style={{fontSize: `20px`}}>
+          We focus collecting and reporting in 5 main areas: education, society,
+          health and culture, participation and employment.
+        </h1>
+        <h1 style={{fontSize: `20px`}}>
+          We recruit local women's organisations to participate so we have
+          accurate information.
+        </h1>
+        <h1>Who we are</h1>
+        <p>
+          Led by Helen Pankhurst, we are a coalition of change-makers committed
+          to seeing a better future for the women of Manchester. ​​
+        </p>
+        <p>
+          As a region that has always led the way and flown the flag for women’s
+          rights, our first initiative, the Pankhurst-Fawcett Scorecard will
+          once again see Greater Manchester leading the charge to bring progress
+          and accountability for women’s rights.
+        </p>
+
         <FullWidthColor>
-          
-            <h1>Scorecard</h1>
-            <p>
-              Our scorecard is named after Emmeline Pankhurst and Millicent
-              Fawcett, two iconic figures who were among the first leaders of the
-              campaign for women’s suffrage. ​
-            </p>
-            <p>
-              The scorecard will be used to conduct annual reviews of gender
-              equality data with the goal of achieving equality across all areas
-              by 2028 - the centenary of equal franchise. ​
-            </p>
-            <Graph />
-          
+          <h1>Scorecard</h1>
+          <p>
+            Our scorecard is named after Emmeline Pankhurst and Millicent
+            Fawcett, two iconic figures who were among the first leaders of the
+            campaign for women’s suffrage. ​
+          </p>
+          <p>
+            The scorecard will be used to conduct annual reviews of gender
+            equality data with the goal of achieving equality across all areas
+            by 2028 - the centenary of equal franchise. ​
+          </p>
+          <Graph />
         </FullWidthColor>
-      {/* Flickity or siema - carousel with quotes */}
-      {/* Add supporters list */}
+        {/* Flickity or siema - carousel with quotes */}
+        {/* Add supporters list */}
       </Layout>
     )
   }
