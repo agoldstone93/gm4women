7b6906f8ea326db5581b09c4e9e905476a079092
Adam Goldstone <amg93@msn.com>
2020-08-21 15:10:27 +0100

    making navbar responsive

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,11 +12,9 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
   }
 `
-
 const Wrapper = styled.div`
   min-height: 100vh;
 `
-
 const Footer = styled.footer`
   text-align: center;
   margin: 24px;
@@ -29,6 +27,41 @@ const StyledMail = styled(Mail)`
   height: 25px;
   color: black;
 `
+const Navbar = styled.ul`
+  list-style: none;
+  margin: 0;
+  align-items: center;
+  display: grid;
+  @media (min-width: 600px) { /* Big Screen */
+    grid-template-columns: 1fr repeat(5, fit-content(20px));
+    gap: 20px;
+  }
+  @media (max-width: 600px) { /* Small Screen */
+    grid-template-columns: auto auto auto repeat(2, fit-content(20px));
+    column-gap: 10px;
+    row-gap: 0px;
+  }
+`
+const LogoItem = styled(Link) `
+  text-shadow: none;
+  background-image: none;
+  box-shadow: none;
+  width: 150px;
+  @media (max-width: 600px) { /* Small Screen */
+    grid-column: 1 / 6;
+    justify-self: center;
+  }
+`
+const StyledItem = styled.li `
+  margin: 0;
+  justify-self: end;
+`
+const ItemTwitter = styled(StyledItem) `
+  
+`
+const ItemEmail = styled(StyledItem) `
+  
+`
 const ListLink = props => (
   <li style={{ margin: `0` }}>
     <Link
@@ -37,7 +70,7 @@ const ListLink = props => (
         color: `black`,
         boxShadow: `none`,
         fontFamily: `Montserrat`,
-        fontWeight: `900`,
+        fontWeight: `900`
       }}
       activeStyle={{
         boxShadow: `0 2.5px 0 indigo`,
@@ -57,16 +90,16 @@ class Layout extends React.Component {
         <GlobalStyle />
         <div style={{ margin: `0.5rem auto`, padding: `0 20px`, maxWidth: `1000px` }}>
           <header style={{ marginBottom: `0.5rem` }}>
-            <ul style={{ listStyle: `none`, margin: `0`, display: `grid`, gridTemplateColumns: `1fr repeat(5, fit-content(20px))`, gap: `20px`, alignItems: `center`}}>
-              <Link to="/" style={{ textShadow: `none`, backgroundImage: `none`, boxShadow: `none` }}>
-                <img src={logo} alt={"GM4Women logo"} style={{ display: `inline`, width: `150px`, padding: `0`, margin: `0` }} />
-              </Link>
+            <Navbar>
+              <LogoItem to="/" >
+                <img src={logo} alt={"GM4Women logo"} style={{  padding: `0`, margin: `0`, maxWidth: `150px` }} />
+              </LogoItem>
               <ListLink to="/">Home</ListLink>
               <ListLink to="/committees">Committees</ListLink>
               <ListLink to="/blog/">Blog</ListLink>
-              <li style={{ margin: `0` }}><a href={"http://www.twitter.com/gm4women2028"} aria-label="Twitter" style={{ boxShadow: `none` }}><StyledTwitter /></a></li>
-              <li style={{ margin: `0` }}><a href={"mailto:gm4women2028@gmail.com"} aria-label="Email" style={{ boxShadow: `none` }}><StyledMail /></a></li>
-            </ul>
+              <ItemTwitter><a href={"http://www.twitter.com/gm4women2028"} aria-label="Twitter" style={{ boxShadow: `none` }}><StyledTwitter /></a></ItemTwitter>
+              <ItemEmail><a href={"mailto:gm4women2028@gmail.com"} aria-label="Email" style={{ boxShadow: `none` }}><StyledMail /></a></ItemEmail>
+            </Navbar>
           </header>
           <main>{children}</main>
         </div>
