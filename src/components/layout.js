679cc2453b8fc306801bb879fd30a3612a03acf0
Adam Goldstone <amg93@msn.com>
2020-08-21 16:16:21 +0100

    removing and refactoring code

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,8 +2,8 @@ import React from "react"
 import { Link } from "gatsby"
 import styled, { createGlobalStyle } from "styled-components"
 import logo from "../../content/assets/GM4women2028_Logo.png"
-import { Twitter } from '@styled-icons/boxicons-logos/'
-import { Mail } from '@styled-icons/ionicons-sharp/'
+import { Twitter } from "@styled-icons/boxicons-logos/"
+import { Mail } from "@styled-icons/ionicons-sharp/"
 
 const GlobalStyle = createGlobalStyle`
   body, html {
@@ -19,7 +19,7 @@ const Footer = styled.footer`
   text-align: center;
   margin: 24px;
 `
-const StyledTwitter = styled(Twitter) `
+const StyledTwitter = styled(Twitter)`
   color: black;
   height: 28px;
 `
@@ -32,35 +32,33 @@ const Navbar = styled.ul`
   margin: 0;
   align-items: center;
   display: grid;
-  @media (min-width: 600px) { /* Big Screen */
+  @media (min-width: 600px) {
+    /* Big Screen */
     grid-template-columns: 1fr repeat(5, fit-content(20px));
     gap: 20px;
   }
-  @media (max-width: 600px) { /* Small Screen */
+  @media (max-width: 600px) {
+    /* Small Screen */
     grid-template-columns: auto auto auto repeat(2, fit-content(20px));
     column-gap: 10px;
     row-gap: 0px;
   }
 `
-const LogoItem = styled(Link) `
+const LogoItem = styled(Link)`
   text-shadow: none;
   background-image: none;
   box-shadow: none;
   width: 150px;
-  @media (max-width: 600px) { /* Small Screen */
+  align-self: center;
+  @media (max-width: 600px) {
+    /* Small Screen */
     grid-column: 1 / 6;
     justify-self: center;
   }
 `
-const StyledItem = styled.li `
+const StyledItem = styled.li`
   margin: 0;
   justify-self: end;
-`
-const ItemTwitter = styled(StyledItem) `
-  
-`
-const ItemEmail = styled(StyledItem) `
-  
 `
 const ListLink = props => (
   <li style={{ margin: `0` }}>
@@ -70,7 +68,7 @@ const ListLink = props => (
         color: `black`,
         boxShadow: `none`,
         fontFamily: `Montserrat`,
-        fontWeight: `900`
+        fontWeight: `900`,
       }}
       activeStyle={{
         boxShadow: `0 2.5px 0 indigo`,
@@ -88,17 +86,45 @@ class Layout extends React.Component {
     return (
       <Wrapper>
         <GlobalStyle />
-        <div style={{ margin: `0.5rem auto`, padding: `0 20px`, maxWidth: `1000px` }}>
+        <div
+          style={{
+            margin: `0.5rem auto`,
+            padding: `0 20px`,
+            maxWidth: `1000px`,
+          }}
+        >
           <header style={{ marginBottom: `0.5rem` }}>
             <Navbar>
-              <LogoItem to="/" >
-                <img src={logo} alt={"GM4Women logo"} style={{  padding: `0`, margin: `0`, maxWidth: `150px` }} />
+              <LogoItem to="/">
+                <img
+                  src={logo}
+                  alt={"GM4Women logo"}
+                  style={{ padding: `0`, margin: `0`, maxWidth: `150px` }}
+                />
               </LogoItem>
               <ListLink to="/">Home</ListLink>
               <ListLink to="/committees">Committees</ListLink>
               <ListLink to="/blog/">Blog</ListLink>
-              <ItemTwitter><a href={"http://www.twitter.com/gm4women2028"} aria-label="Twitter" style={{ boxShadow: `none` }}><StyledTwitter /></a></ItemTwitter>
-              <ItemEmail><a href={"mailto:gm4women2028@gmail.com"} aria-label="Email" style={{ boxShadow: `none` }}><StyledMail /></a></ItemEmail>
+              <StyledItem>
+                <a
+                  href={"http://www.twitter.com/gm4women2028"}
+                  aria-label="Twitter"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{ boxShadow: `none` }}
+                >
+                  <StyledTwitter />
+                </a>
+              </StyledItem>
+              <StyledItem>
+                <a
+                  href={"mailto:gm4women2028@gmail.com"}
+                  aria-label="Email"
+                  style={{ boxShadow: `none` }}
+                >
+                  <StyledMail />
+                </a>
+              </StyledItem>
             </Navbar>
           </header>
           <main>{children}</main>
